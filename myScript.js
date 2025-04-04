b6c3a3c17cd8bb416ae056475d7441c1226a8db7
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2025-04-04 23:46:53 +0500

    Sultan IT WEB & Generative AI
    Sultan IT WEB & Generative AI

---
+++ myScript.js
@@ -0,0 +1,15 @@
+
+// New   New
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        themeToggle.textContent = '☀️ Light Mode';
+    } else {
+        themeToggle.textContent = '🌙 Dark Mode';
+    }
+});
+
+
